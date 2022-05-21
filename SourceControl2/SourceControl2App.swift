2ae22fcbcefbc7cad9aa4767f58d6e7f904d55b2
Juan Carlos Ruge <juanc81@gmail.com>
2022-05-20 20:17:20 -0400

    Initial Commit

---
+++ SourceControl2/SourceControl2App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControl2App.swift
+//  SourceControl2
+//
+//  Created by Juan Ruge on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
